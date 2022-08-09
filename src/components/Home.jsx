f1fb5b3b484efad30ebb22cefd996823662681cc
anandjha8719 <anandjha8719@gmail.com>
2022-08-09 17:55:47 +0530

    added localstorage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,59 +1,35 @@
-import React from "react";
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis } from "recharts";
-
-const pdata = [
-    {
-        productKey: 1,
-        accuracy: 95
-    },
-    {
-        productKey: 2,
-        accuracy: 88
-    },
-    {
-        productKey: 3,
-        accuracy: 78
-    },
-    {
-        productKey: 4,
-        accuracy: 84
-    },
-    {
-        productKey: 5,
-        accuracy: 100
-    },
-    {
-        productKey: 6,
-        accuracy: 94
-    },
-    {
-        productKey: 7,
-        accuracy: 84
-    },
-    {
-        productKey: 8,
-        accuracy: 76
-    },
-    {
-        productKey: 9,
-        accuracy: 88
-    },
-    {
-        productKey: 10,
-        accuracy: 91
-    },
-]
+import { useState, useEffect } from "react";
+import { useLocalStorage } from "../useLocalStorage";
 
 export const Home = () => {
-    return <div>
-        <h1>Home</h1>
-        <ResponsiveContainer width="100%" aspect={3}>
-        <LineChart data={pdata}>
-            <XAxis dataKey="productKey" interval={'preserveStartEnd'}/>
-            <YAxis dataKey="accuracy" interval={'preserveStartEnd'}/>
-            <Line dataKey="accuracy" />
-        </LineChart>
-        </ResponsiveContainer>
+//   const [name, setName] = useState(() => {
+//     getting stored value
+//     const saved = localStorage.getItem("name");
+//     const initialValue = JSON.parse(saved);
+//     return initialValue || "";
+//   });
 
+//   useEffect(() => {
+//     // storing input name
+//     localStorage.setItem("name", JSON.stringify(name));
+//   }, [name]);
+const [name, setName] = useLocalStorage("name", "");
+
+  return (
+    <div>
+        <form>
+        <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Full name"
+            aria-label="fullname"
+        />
+        <input type="submit" value="Submit"></input>
+        </form>
     </div>
-};
+    
+  );
+
+
+}
--- src/components/ParseExcel.jsx
+++ src/components/ParseExcel.jsx
@@ -1,11 +1,59 @@
 import React, {useState} from "react";
 import * as XLSX from 'xlsx'
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis } from "recharts";
+import { useLocalStorage } from "../useLocalStorage";
+
+const pdata = [
+    {
+        productKey: 1,
+        accuracy: 95
+    },
+    {
+        productKey: 2,
+        accuracy: 88
+    },
+    {
+        productKey: 3,
+        accuracy: 78
+    },
+    {
+        productKey: 4,
+        accuracy: 84
+    },
+    {
+        productKey: 5,
+        accuracy: 100
+    },
+    {
+        productKey: 6,
+        accuracy: 94
+    },
+    {
+        productKey: 7,
+        accuracy: 84
+    },
+    {
+        productKey: 8,
+        accuracy: 76
+    },
+    {
+        productKey: 9,
+        accuracy: 88
+    },
+    {
+        productKey: 10,
+        accuracy: 91
+    },
+]
+
 export const ParseExcel = () => {
     const[fileName, setFileName] = useState(null);
     // const [ finalArray, setFinalArray] = useState([{
     //     productKey: null,
     //     accuracy: null
     // }]);
+    const [name, setName] = useLocalStorage("name", "");
+    
 
     const handleFile = async (e) => {
         const file = e.target.files[0];
@@ -31,11 +79,24 @@ export const ParseExcel = () => {
     return (
     <div>
         <h1>Parse excel bro</h1>
+            <p>
+                UserName: <span>{name}</span>
+            </p>
         
             <p>
                 Filename:- <span>{fileName}</span>
                 
             </p>
         <input type="file" onChange={(e) => handleFile(e)} />
+        <div>
+        <ResponsiveContainer width="80%" aspect={3}>
+            <LineChart data={pdata}>
+                <XAxis dataKey="productKey" interval={'preserveStartEnd'}/>
+                <YAxis dataKey="accuracy" interval={'preserveStartEnd'}/>
+                <Line dataKey="accuracy" />
+            </LineChart>
+        </ResponsiveContainer>
+
+    </div>
     </div>)
 };
